79a3616ef569b2591642e67ac89ad06075355b8f
mor3hate <thelokkedmail3@gmail.com>
2022-12-18 21:08:04 +0300

    Del(uploads directory)

--- front/app/components/screens/auth/Auth.tsx
+++ front/app/components/screens/auth/Auth.tsx
@@ -1,68 +1,71 @@
-import { useAuth } from '@/hooks/useAuth'
-import { FC, useState } from 'react'
-import { SubmitHandler, useForm } from 'react-hook-form'
-import { IAuthInput } from './auth.interface'
-import { useAuthRedirect } from './useAuthRedirect'
+import {useAuth} from '@/hooks/useAuth'
+import {FC, useState} from 'react'
+import {SubmitHandler, useForm} from 'react-hook-form'
+import {IAuthInput} from './auth.interface'
+import {useAuthRedirect} from './useAuthRedirect'
 
 import styles from './Auth.module.scss'
 import Meta from '@/utils/meta/Meta'
 import ButtonAuth from '@/components/ui/button/ButtonAuth'
 import AuthFields from './AuthFields'
-import { useActions } from '@/hooks/useActions'
+import {useActions} from '@/hooks/useActions'
 
 const Auth: FC = () => {
-	useAuthRedirect()
+    useAuthRedirect()
 
-	const { isLoading } = useAuth()
+    const {isLoading} = useAuth()
 
-	const [type, setType] = useState<'login' | 'register'>('login')
+    const [type, setType] = useState<'login' | 'register'>('login')
 
-	const {
-		register: registerInput,
-		handleSubmit,
-		formState,
-		reset,
-	} = useForm<IAuthInput>({
-		mode: 'onChange',
-	})
+    const {
+        register: registerInput,
+        handleSubmit,
+        formState,
+        reset,
+    } = useForm<IAuthInput>({
+        mode: 'onChange',
+    })
 
-	const { register, login } = useActions()
+    const {register, login} = useActions()
 
-	const onSubmit: SubmitHandler<IAuthInput> = data => {
-		if (type === 'login') login(data)
-		else if (type === 'register') register(data)
+    const onSubmit: SubmitHandler<IAuthInput> = data => {
+        if (type === 'login') login(data)
+        else if (type === 'register') register(data)
 
-		reset()
-	}
+        reset()
+    }
 
-	return (
-		<Meta title='Auth'>
-			<section className={styles.wrapper}>
-				<form onSubmit={handleSubmit(onSubmit)}>
-					<h1>Authorization</h1>
+    return (
+        <Meta title='Auth'>
+            <section className={styles.wrapper}>
+                <form onSubmit={handleSubmit(onSubmit)}>
+                    <h1>Authorization</h1>
 
-					<AuthFields
-						formState={formState}
-						register={registerInput}
-						isPasswordRequired
-					/>
+                    <AuthFields
+                        formState={formState}
+                        register={registerInput}
+                        isPasswordRequired
+                    />
 
-					<div className={styles.buttons_wrapper}>
-						<ButtonAuth
-							onClick={() => setType('login')}
-							disabled={isLoading}
-							text='Login'
-						/>
-						<ButtonAuth
-							onClick={() => setType('register')}
-							disabled={isLoading}
-							text='Register'
-						/>
-					</div>
-				</form>
-			</section>
-		</Meta>
-	)
+                    <div className={styles.buttons_wrapper}>
+                        <ButtonAuth
+                            onClick={() => setType('login')}
+                            disabled={isLoading}
+                            text='Login'
+                        />
+                        <ButtonAuth
+                            onClick={() => setType('register')}
+                            disabled={isLoading}
+                            text='Register'
+                        />
+                    </div>
+                </form>
+            </section>
+        </Meta>
+    )
 }
+
+
+
 
 export default Auth
